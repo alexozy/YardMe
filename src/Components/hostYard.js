30cb0430a4fed229e6001a3db23f1992f6777f95
Ozanne Alexander <alexanoz@outlook.com>
2023-04-06 21:56:51 -0400

    fixed hostYard form options

--- src/Components/hostYard.js
+++ src/Components/hostYard.js
@@ -1,10 +1,52 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function FormFloatingSelectExample() {
+function hostYardListing() {
   return (
 
-    
+    <Form>
+    <Row className="mb-3">
+      <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Row>
+
+    <Form.Group className="mb-3" controlId="formGridAddress1">
+      <Form.Label>Address</Form.Label>
+      <Form.Control placeholder="1234 Main St" />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formGridAddress2">
+      <Form.Label>Address 2</Form.Label>
+      <Form.Control placeholder="Apartment, studio, or floor" />
+    </Form.Group>
+
+    <Row className="mb-3">
+      <Form.Group as={Col} controlId="formGridCity">
+        <Form.Label>City</Form.Label>
+        <Form.Control />
+      </Form.Group>
+      <Form.Group as={Col} controlId="formGridCity">
+        <Form.Label> State </Form.Label>
+        <Form.Control />
+      </Form.Group>
+     
+
+      <Form.Group as={Col} controlId="formGridZip">
+        <Form.Label>Zip</Form.Label>
+        <Form.Control />
+      </Form.Group>
+    </Row>
+
     <FloatingLabel controlId="floatingSelect" label="Works with selects">
       <Form.Select aria-label="Floating label select example">
         <option>Yard Features</option>
@@ -13,11 +55,14 @@ function FormFloatingSelectExample() {
         <option value="3">Frolick Field</option>
         <option value="4">Arena</option>
         <option value="5">None/Other</option>
-
-      
       </Form.Select>
     </FloatingLabel>
+
+    <Button variant="primary" type="submit">
+      Create!
+    </Button>
+  </Form>
   );
 }
 
-export default FormFloatingSelectExample;
+export default hostYardListing;
