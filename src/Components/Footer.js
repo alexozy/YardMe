b35f1c4f7466a0f7a16305659225e8e93a807dfc
Ozanne Alexander <alexanoz@outlook.com>
2023-04-06 22:30:38 -0400

    added yard me footer example and products column

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -30,7 +30,35 @@ export default function App() {
           </a>
         </div>
       </section>
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <MDBIcon icon="gem" className="me-3" />
+                YardMe Host
+              </h6>
+              <p>
+                We created YardMe Host for homeowners who have a lot of yard and wanted to make extra cash! 
+                We are still in the developing stages and can't wait to grow!
+              </p>
+            </MDBCol>
 
+            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  YardMe
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  YardMe Host
+                </a>
+              </p>
+            </MDBCol>
+
+            
       
     </MDBFooter>
   );
