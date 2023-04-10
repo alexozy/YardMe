2a9310fde449b3cca10259f83ed44cd7688d7c64
Ozanne Alexander <alexanoz@outlook.com>
2023-04-10 19:09:02 -0400

    navbar color

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,7 +11,7 @@ import {
 
 function yardNav() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="success" expand="lg">
       <Container>
       {/* navbar logo code for image import */}
         <MDBNavbarBrand href='#'>
@@ -23,15 +23,15 @@ function yardNav() {
             />
           </MDBNavbarBrand>
 
-        <Navbar.Brand href="#home">YardMe: Host Platform</Navbar.Brand>
+        <Navbar.Brand href="#home">YardMe:Host Platform</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">MyYards</Nav.Link>
-            <Nav.Link href="#link">PlaceHolder</Nav.Link>
+            <Nav.Link href="#link">HostYard</Nav.Link>
             <NavDropdown title="Options" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">HostYard</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">HostNew</NavDropdown.Item>
+              {/* <NavDropdown.Item href="#action/3.1">HostYard</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">HostNew</NavDropdown.Item> */}
               <NavDropdown.Item href="#action/3.3">LogOut</NavDropdown.Item>
             </NavDropdown>
           </Nav>
