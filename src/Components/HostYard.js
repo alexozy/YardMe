5c0649624a15b0b985c23e7299e45c8048e0bce3
Abigail Henderson <abigailhenderson@Abigails-MacBook-Air.local>
2023-04-14 02:26:20 -0400

    Added photos corresponding to drop down menu options for yard features

--- src/Components/HostYard.js
+++ src/Components/HostYard.js
@@ -1,69 +1,80 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-//test test
-function HostYardListing() {
+import { useState } from 'react';
+import { FloatingLabel, Form, Button, Col, Row } from 'react-bootstrap';
+
+const HostYardListing = () => {
+  const [image, setImage] = useState('');
+  const img1 = 'https://www.outdooressentialproducts.com/-/media/OutdoorEssentialsV3/Products/Wood-Fence/Fence-Components/Pickets_LIFESTYLE.jpg'
+  const img2 = 'https://ww1.prweb.com/prfiles/2013/10/30/11286244/Full%20Course%201.JPG'
+  const img3 = 'https://media.istockphoto.com/id/827580726/photo/landscape-of-grass-field-and-green-environment-public-park-use-as-natural-background-backdrop.jpg?s=612x612&w=0&k=20&c=qUacNPfzvoW-20ELsIb9AnFeW4yOhqs5xZqjOlBODl4='
+  const img4 = 'https://images.squarespace-cdn.com/content/v1/5c35741ecef37214f2bde4d5/84c50d1f-c796-49dd-99f8-15db5ddfd6e7/tempImage5NNObF.jpg?format=1000w'
+  const img5 = ''
+
+  const handleImage = (value) => {
+    value.toString() === "1" && setImage(img1)
+    value.toString() === "2" && setImage(img2)
+    value.toString() === "3" && setImage(img3)
+    value.toString() === "4" && setImage(img4)
+    value.toString() === "5" && setImage(img5)
+  }
+
   return (
     <div>
-    <Form>
-    <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridEmail">
-        <Form.Label>Name Your Yard !</Form.Label>
-        <Form.Control type="email" placeholder="i.e. Frolic Farm" />
-      </Form.Group>
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Name Your Yard!</Form.Label>
+            <Form.Control type="email" placeholder="i.e. Frolic Farm" />
+          </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridPassword">
-        <Form.Label>Hourly Cost</Form.Label>
-        <Form.Control type="password" placeholder="Enter Dollar Amount Only" />
-      </Form.Group>
-    </Row>
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Hourly Cost</Form.Label>
+            <Form.Control type="password" placeholder="Enter Dollar Amount Only" />
+          </Form.Group>
+        </Row>
 
-    <Form.Group className="mb-3" controlId="formGridAddress1">
-      <Form.Label>Address</Form.Label>
-      <Form.Control placeholder="1234 Main St" />
-    </Form.Group>
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
 
-    <Form.Group className="mb-3" controlId="formGridAddress2">
-      <Form.Label>Address 2</Form.Label>
-      <Form.Control placeholder="Apartment, studio, or floor" />
-    </Form.Group>
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Address 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
 
-    <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridCity">
-        <Form.Label>City</Form.Label>
-        <Form.Control />
-      </Form.Group>
-      <Form.Group as={Col} controlId="formGridCity">
-        <Form.Label> State </Form.Label>
-        <Form.Control />
-      </Form.Group>
-     
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label> State </Form.Label>
+            <Form.Control />
+          </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridZip">
-        <Form.Label>Zip</Form.Label>
-        <Form.Control />
-      </Form.Group>
-    </Row>
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Row>
 
-    <FloatingLabel controlId="floatingSelect" label="Works with selects">
-      <Form.Select aria-label="Floating label select example">
-        <option>Yard Features</option>
-        <option value="1">Fully-Fenced</option>
-        <option value="2">Agility Course</option>
-        <option value="3">Frolick Field</option>
-        <option value="4">Arena</option>
-        <option value="5">None/Other</option>
-      </Form.Select>
-    </FloatingLabel>
-
-    <Button variant="success" type="submit">
-      Create!
-    </Button>
-  </Form>
-  </div>
+        <FloatingLabel controlId="floatingSelect" label="Yard Features">
+          <Form.Select onChange={(e) => handleImage(e.target.value)} aria-label="Floating label select example">
+            <option value={1}>Fully-Fenced</option>
+            <option value={2}>Agility Course</option>
+            <option value={3}>Frolic Field</option>
+            <option value={4}>Arena</option>
+            <option value={5}>None/Other</option>
+          </Form.Select>
+        </FloatingLabel>
+        <img style={{borderRadius: '10px'}} src={image} alt='text' height='300px' width='400px'/>
+        <br />
+        <Button variant="success" type="submit">
+          Create!
+        </Button>
+      </Form>
+    </div>
   );
 }
 
-export default HostYardListing;
+export default HostYardListing;       
