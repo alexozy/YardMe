3a30e0ebb00e20dc95558763f7d098b02a9c1d6e
Abigail Henderson <abigailhenderson@Abigails-MacBook-Air.local>
2023-04-15 17:50:29 -0400

    corrected errors

--- src/Components/HostYard.js
+++ src/Components/HostYard.js
@@ -3,18 +3,18 @@ import { FloatingLabel, Form, Button, Col, Row } from 'react-bootstrap';
 
 const HostYardListing = () => {
   const [image, setImage] = useState('');
-  const img1 = 'https://www.outdooressentialproducts.com/-/media/OutdoorEssentialsV3/Products/Wood-Fence/Fence-Components/Pickets_LIFESTYLE.jpg'
+  const img1 = ''
   const img2 = 'https://ww1.prweb.com/prfiles/2013/10/30/11286244/Full%20Course%201.JPG'
   const img3 = 'https://media.istockphoto.com/id/827580726/photo/landscape-of-grass-field-and-green-environment-public-park-use-as-natural-background-backdrop.jpg?s=612x612&w=0&k=20&c=qUacNPfzvoW-20ELsIb9AnFeW4yOhqs5xZqjOlBODl4='
   const img4 = 'https://images.squarespace-cdn.com/content/v1/5c35741ecef37214f2bde4d5/84c50d1f-c796-49dd-99f8-15db5ddfd6e7/tempImage5NNObF.jpg?format=1000w'
-  const img5 = ''
+  const img5 = 'https://www.outdooressentialproducts.com/-/media/OutdoorEssentialsV3/Products/Wood-Fence/Fence-Components/Pickets_LIFESTYLE.jpg'
 
-  const handleImage = (value) => {
-    value.toString() === "1" && setImage(img1)
-    value.toString() === "2" && setImage(img2)
-    value.toString() === "3" && setImage(img3)
-    value.toString() === "4" && setImage(img4)
-    value.toString() === "5" && setImage(img5)
+  function handleImage(value) {
+    value.toString() === "1" && setImage(img1);
+    value.toString() === "2" && setImage(img2);
+    value.toString() === "3" && setImage(img3);
+    value.toString() === "4" && setImage(img4);
+    value.toString() === "5" && setImage(img5);
   }
 
   return (
@@ -60,14 +60,14 @@ const HostYardListing = () => {
 
         <FloatingLabel controlId="floatingSelect" label="Yard Features">
           <Form.Select onChange={(e) => handleImage(e.target.value)} aria-label="Floating label select example">
-            <option value={1}>Fully-Fenced</option>
+            <option value={1}>None/Other</option>
             <option value={2}>Agility Course</option>
             <option value={3}>Frolic Field</option>
             <option value={4}>Arena</option>
-            <option value={5}>None/Other</option>
+            <option value={5}>Fully-Fenced</option>
           </Form.Select>
         </FloatingLabel>
-        <img style={{borderRadius: '10px'}} src={image} alt='text' height='300px' width='400px'/>
+        <img style={{borderRadius: '10px'}} src={image} alt='Choose the feature above closest to your yard' height='300px' width='400px'/>
         <br />
         <Button variant="success" type="submit">
           Create!
@@ -78,3 +78,20 @@ const HostYardListing = () => {
 }
 
 export default HostYardListing;       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
